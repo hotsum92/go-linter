bb647579659003eb4323e7589dffb87a6c2afb5c
832022 <hotsuma_kawashima@densan-s.co.jp>
2024-06-21 09:14:20 +0900

    implement go/ast parser

--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"go/ast"
+	"go/parser"
+	"go/token"
+)
+
+func Parse(filename string) error {
+	fset := token.NewFileSet()
+	f, err := parser.ParseFile(fset, filename, nil, parser.Mode(0))
+
+	if err != nil {
+		return err
+	}
+
+	for _, d := range f.Decls {
+		ast.Print(fset, d)
+		fmt.Println()
+	}
+	return nil
+}
 
 func main() {
-	fmt.Println("vim-go")
+	Parse("sample/main.go")
 }
